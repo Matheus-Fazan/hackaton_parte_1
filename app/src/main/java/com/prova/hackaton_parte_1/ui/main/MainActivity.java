3f6a1d0d1f2540c396ad445ded599caab6c03ab7
Nicolas25vlad <nicolas25vlad@proton.me>
2026-09-08 08:11:49 -0300

    refactor: organiza tela inicial em ui

--- app/src/main/java/com/prova/hackaton_parte_1/ui/main/MainActivity.java
+++ app/src/main/java/com/prova/hackaton_parte_1/ui/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.prova.hackaton_parte_1;
+package com.prova.hackaton_parte_1.ui.main;
 
 import android.os.Bundle;
 
@@ -8,16 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.prova.hackaton_parte_1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (view, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
